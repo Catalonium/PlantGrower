eced849315ee57a5da20781bd341c13bdebd9ced
A. Bulut Catikoglu <bulutcatikoglu@gmail.com>
2019-02-14 21:42:05 +0900

    Classes setup, gitignore update
    - Basic custom class structure created
    - Custom classes WIP
    - gitignore update

--- Assets/Scripts/Field.cs
+++ Assets/Scripts/Field.cs
@@ -1,16 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Field : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public Crop currentCrop;
+	bool isWatered;
+	int timer;
+
+	void Start() {
+		// set timer and shit
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void Update() {
+		// if clicked with a crop, assign it to currentCrop
+		// assign growTime to timer
+
+		// if clicked with pot, isWatered true
+		// start timer count down
+
+		// if timer ends, reset the field and add grown plant's score to background scores
 	}
+
 }
---
+++ Assets/Scripts/MouseMechanics.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class MouseMechanics : MonoBehaviour {
+
+	// variable to hold clicked object 
+
+	void Start() {
+		// init mouse position assignment
+	}
+
+	void FixedUpdate() {
+		// update mouse position
+
+		// select clicked object and carry it with cursor
+
+		// when clicked with an object on the cursor, reset it
+	}
+
+}
+
