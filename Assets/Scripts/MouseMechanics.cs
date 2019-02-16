b6298bcc58a2f838707e976a0a081c747a5ee5b9
Nyein Chan Aung <developernca@gmail.com>
2019-02-16 23:16:54 +0900

    Check mouse click on seeds.

--- Assets/Scripts/MouseMechanics.cs
+++ Assets/Scripts/MouseMechanics.cs
@@ -2,19 +2,42 @@ using UnityEngine;
 
 public class MouseMechanics : MonoBehaviour {
 
-	// variable to hold clicked object 
+    // Main Camera
+    public Camera mainCam;
 
-	void Start() {
-		// init mouse position assignment
-	}
+    // Constant values for primary mouse button
+    private const int PRIMARY_MOUSE_BTN = 0;
 
-	void FixedUpdate() {
-		// update mouse position
+    // variable to hold clicked object 
 
-		// select clicked object and carry it with cursor
+    void Start()
+    {
+        // init mouse position assignment
+    }
 
-		// when clicked with an object on the cursor, reset it
-	}
+    private void Update()
+    {
+        Vector3 clickPos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+        if (Input.GetMouseButtonDown(PRIMARY_MOUSE_BTN))
+        {
+            Debug.Log("Mouse clicked .... ");
+            // get mouse click position in world unit(not in actual pixel unit)
+            Collider2D collider2D = Physics2D.OverlapPoint(new Vector2(clickPos.x, clickPos.y));
+            if (collider2D != null && collider2D.CompareTag("TagSeedEggPlant"))
+            {
+                Debug.Log("Clicked on the Eggplant seeds...");
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // update mouse position
+
+        // select clicked object and carry it with cursor
+
+        // when clicked with an object on the cursor, reset it
+    }
 
 }
 
