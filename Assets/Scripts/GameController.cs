80b9bbafc9d84bd74a33e61666690bfef5acbbc5
A. Bulut Catikoglu <bulutcatikoglu@gmail.com>
2019-03-10 13:31:10 +0900

    v1.0 feature-complete, menu interactions, various code-related fixes
    - Project is now feature-complete, build-ready
    - Menu interactions now work as intended
    - Various code-related fixes and refactoring
    - Project info data finalized

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+	// variables
+	public int currentScore = 0;
+	public int highScore = 0;
+
+	// ref objects
+	public GameObject _game;
+	public GameObject _menuButtons;
+	public Text _menuCurrScore;
+	public Text _menuHighScore;
+	public Text _version;
+	public Text _credits;
+
+	// Use this for initialization
+	void Start () {
+		// Version init at startup
+		_version.text = Application.companyName + "-" + Application.productName + "_v" + Application.version;
+		// Credits init at startup
+		_credits.text = "by A. Bulut Catikoglu and Nyein Chan Aung";
+		// Manage saved score
+		if (!PlayerPrefs.HasKey("HighScore")) {
+			PlayerPrefs.SetInt("HighScore", 0);
+		}
+		else {
+			highScore = PlayerPrefs.GetInt("HighScore");
+		}
+		// Manage saved startup state
+		if (!PlayerPrefs.HasKey("isStartup") || PlayerPrefs.GetInt("isStartup").Equals(0)) {
+			PlayerPrefs.SetInt("isStartup", 1);
+			endGame(false);
+		}
+	}
+
+	public void endGame(bool isGameOver = false) {
+		if (isGameOver) {
+			currentScore = GameObject.FindWithTag("Game_Score").GetComponent<Score>().score;
+			if (highScore < currentScore) {
+				PlayerPrefs.SetInt("HighScore", currentScore);
+				highScore = currentScore;
+			}
+		}
+		_game.SetActive(false);
+		showMenu();
+	}
+
+	public void showMenu() {
+		_menuButtons.SetActive(true);
+		_menuCurrScore.gameObject.SetActive(true);
+		_menuCurrScore.text = "Current Score: " + currentScore;
+		_menuHighScore.gameObject.SetActive(true);
+		_menuHighScore.text = "High Score: " + highScore;
+	}
+
+	public void startGame() {
+		SceneManager.LoadScene(0);
+	}
+
+	public void clearHighScore() {
+		PlayerPrefs.SetInt("HighScore", 0);
+	}
+	
+	public void quitGame() {
+		PlayerPrefs.SetInt("isStartup", 0);
+		Application.Quit();
+	}
+
+}
